6928dd208f5bb8c1899cf13fe41d95c2e6865238
f.idolenkov <idolenkov@gmail.com>
2024-12-14 16:46:41 +0300

    add gz ev time from

--- test/test_journal_admin_get_by_filter.py
+++ test/test_journal_admin_get_by_filter.py
@@ -21,6 +21,22 @@ class TestPosAdminGetByFilter:
         get_by_filter.check_status_code(200)
         get_by_filter.compare_response(serial_sender.final_page)
 
+    @pytest.mark.parametrize('eventTimeTo', [
+        0,
+        4294967295
+    ])
+    def test_gz_event_time_to(self, eventTimeTo, allure_attach_request_n_response_body):
+        serial_sender = SerialSender('adm', ext_data={'event_time_to': eventTimeTo, 'event_time_from': 0})
+        serial_sender.send_requests(5)
+        serial_sender.create_custom_page()
+        get_by_filter = AdminGetByFilter()
+        get_by_filter.send_request(serial_sender.payload_filter, serial_sender.page_params)
+        allure_attach_request_n_response_body(str(serial_sender.payload_filter), get_by_filter.response.text,
+                                              request_params=serial_sender.page_params,
+                                              status_code=get_by_filter.response.status_code)
+        get_by_filter.check_status_code(200)
+        get_by_filter.compare_response(serial_sender.final_page)
+
 
 
 
