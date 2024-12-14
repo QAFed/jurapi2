5cd35209c4f4bdf5136e951df31a735175db0401
f.idolenkov <idolenkov@gmail.com>
2024-12-14 16:56:42 +0300

    add gz action type

--- test/test_journal_admin_get_by_filter.py
+++ test/test_journal_admin_get_by_filter.py
@@ -38,6 +38,21 @@ class TestPosAdminGetByFilter:
         get_by_filter.compare_response(serial_sender.final_page)
 
 
-
+    @pytest.mark.parametrize('actionType',[
+        -2147483647,
+        0,
+        2147483647
+    ])
+    def test_gz_action_type(self, actionType, allure_attach_request_n_response_body):
+        serial_sender = SerialSender('adm', ext_data={'action_type': actionType})
+        serial_sender.send_requests(5)
+        serial_sender.create_custom_page()
+        get_by_filter = AdminGetByFilter()
+        get_by_filter.send_request(serial_sender.payload_filter, serial_sender.page_params)
+        allure_attach_request_n_response_body(str(serial_sender.payload_filter), get_by_filter.response.text,
+                                              request_params=serial_sender.page_params,
+                                              status_code=get_by_filter.response.status_code)
+        get_by_filter.check_status_code(200)
+        get_by_filter.compare_response(serial_sender.final_page)
 
 
